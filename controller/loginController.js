de17959b0d15d9769e1456d507c1f09c6fd2e5d4
Amanullha Zoha <amanullhazoha3784@gmail.com>
2024-10-02 09:55:42 +0600

    issue solve

--- controller/loginController.js
+++ controller/loginController.js
@@ -60,6 +60,8 @@ const login = async (req, res, next) => {
 
     res.locals.loggedInUser = payload;
 
+    console.log("Cookie Set:", res.getHeader("Set-Cookie"));
+
     const now = new Date();
     await User.findByIdAndUpdate(user._id, { lastActivity: now });
 
