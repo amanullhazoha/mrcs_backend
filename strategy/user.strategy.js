a3a00910400fa5de7542cb740d81a64df71b1c4d
Amanullha Zoha <amanullhazoha3784@gmail.com>
2024-10-10 22:00:27 +0600

    issue sovle

--- strategy/user.strategy.js
+++ strategy/user.strategy.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const passport = require("passport");
 const User = require("../models/People");
 const { Strategy } = require("passport-jwt");
@@ -6,9 +7,15 @@ module.exports = () => {
   const cookieExtractor = (req) => {
     let token = null;
 
-    if (req && req?.headers?.authorization) {
-      token = req.headers?.authorization?.replace("Bearer ", "");
+    console.log(req.headers?.authorization);
+
+    if (req && req.signedCookies) {
+      token = req.signedCookies[process.env.COOKIE_NAME];
     }
+
+    // if (req && req?.headers?.authorization) {
+    //   token = req.headers?.authorization?.replace("Bearer ", "");
+    // }
 
     return token;
   };
